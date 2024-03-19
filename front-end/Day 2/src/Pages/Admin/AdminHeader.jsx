6e7bb3ba4ab2571f7369eae66a720f49c8a3eb51
Ranjith000777 <104686384+Ranjith000777@users.noreply.github.com>
2024-03-19 12:23:13 +0530

    Add files via upload

---
+++ front-end/Day 2/src/Pages/Admin/AdminHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../assets/css/AdminCss/AdminHeader.css'
+import { useNavigate } from 'react-router-dom'
+
+const AdminHeader = () => {
+  
+  return (
+    <div className='admin-header-container'>
+            <h1>TECHTITAN <span>Admission Center</span></h1>
+            
+        </div>
+  )
+}
+
+export default AdminHeader
